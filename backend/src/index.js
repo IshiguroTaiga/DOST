e26f6ea0d222db84382ef737d09df0c0b91bccfb
Micko <mickogabriel75@gmail.com>
2026-06-10 15:45:30 +0800

    add entry fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -58,6 +58,10 @@ io.on('connection', (socket) => {
 });
 
 // --- Middleware ---
+app.use((req, res, next) => {
+  console.log(`[Request] ${req.method} ${req.url}`);
+  next();
+});
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use('/uploads', express.static(uploadDir));
